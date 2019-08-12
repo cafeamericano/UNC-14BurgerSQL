e7ca253669d4fb9138ba6429ddd9b138ed40ee3f
Matthew <mfarmer5102@gmail.com>
2019-08-12 13:15:46 -0400

    Front end now communicating with back end via ORM

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -5,102 +5,106 @@ var router = express.Router()
 
 //GET ROUTES ####################################################################################
 
-router.get("/orm/allburgers", function (req, res) {
-    burger.all(function (data) {
-        res.json(data)
-    });
-});
-
-router.get("/orm/isdevoured", function (req, res) {
-    burger.findDevoured(function (data) {
-        res.json(data)
-    });
-});
-
-router.get("/orm/isnotdevoured", function (req, res) {
-    burger.findNotDevoured(function (data) {
-        res.json(data)
-    });
-});
-
-router.post('/orm/addnewburger', function (req, res) {
-    burger.addNew(req.body.burgerName, function (data) {
-        res.json(data)
-    });
-})
-
-router.put('/orm/changedevouredstatus', function (req, res) {
-    console.log(req.body)
-    burger.update(req.body.id, req.body.newdevouredstatus, function (data) {
-        res.json(data)
-    });
-})
-
-router.delete('/orm/deleteaburger', function (req, res) {
-    console.log(req.body.id)
-    burger.delete(req.body.id, function (data) {
-        res.json(data)
-    });
-})
-
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
 router.get('/', function (req, res) {
     res.redirect('/api/allburgers')
 })
 
-router.get('/api/allburgers', function (req, res) {
-    query = `SELECT * FROM burgers;`
-    connection.query(query, function (err, result) {
+router.get("/api/allburgers", function (req, res) {
+    burger.all(function (result) {
         res.render('index', { datum: result })
     });
-})
+});
 
-router.get('/api/isdevoured', function (req, res) {
-    query = `SELECT * FROM burgers WHERE devoured=true;`
-    connection.query(query, function (err, result) {
+router.get("/api/isdevoured", function (req, res) {
+    burger.findDevoured(function (result) {
         res.render('index', { datum: result })
     });
-})
+});
 
-router.get('/api/isnotdevoured', function (req, res) {
-    query = `SELECT * FROM burgers WHERE devoured=false;`
-    connection.query(query, function (err, result) {
+router.get("/api/isnotdevoured", function (req, res) {
+    burger.findNotDevoured(function (result) {
         res.render('index', { datum: result })
     });
-})
+});
 
-//POST ROUTES ####################################################################################
-
-//Add new burger
 router.post('/api/addnewburger', function (req, res) {
-    query = `INSERT INTO burgers (burger_name, devoured) VALUES ('${req.body.burgerName}', false);`
-    connection.query(query, function (err, result) {
+    burger.addNew(req.body.burgerName, function (result) {
         res.send('Burger added.')
     });
 })
 
-//PUT ROUTES ####################################################################################
-
-//Change devoured status
 router.put('/api/changedevouredstatus', function (req, res) {
-    console.log(query)
-    query = `UPDATE burgers SET devoured=${req.body.newdevouredstatus} WHERE id='${req.body.id}';`
-    connection.query(query, function (err, result) {
-        res.send('Burger devoured status updated.')
+    console.log(req.body)
+    burger.update(req.body.id, req.body.newdevouredstatus, function (result) {
+        res.send('Burger updated.')
     });
 })
 
-//DELETE ROUTES ####################################################################################
-
-//Delete a burger
 router.delete('/api/deleteaburger', function (req, res) {
-    query = `DELETE FROM burgers WHERE id='${req.body.id}';`
-    connection.query(query, function (err, result) {
+    console.log(req.body.id)
+    burger.delete(req.body.id, function (result) {
         res.send('Burger deleted.')
     });
 })
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// router.get('/', function (req, res) {
+//     res.redirect('/api/allburgers')
+// })
+
+// router.get('/api/allburgers', function (req, res) {
+//     query = `SELECT * FROM burgers;`
+//     connection.query(query, function (err, result) {
+//         res.render('index', { datum: result })
+//     });
+// })
+
+// router.get('/api/isdevoured', function (req, res) {
+//     query = `SELECT * FROM burgers WHERE devoured=true;`
+//     connection.query(query, function (err, result) {
+//         res.render('index', { datum: result })
+//     });
+// })
+
+// router.get('/api/isnotdevoured', function (req, res) {
+//     query = `SELECT * FROM burgers WHERE devoured=false;`
+//     connection.query(query, function (err, result) {
+//         res.render('index', { datum: result })
+//     });
+// })
+
+// //POST ROUTES ####################################################################################
+
+// //Add new burger
+// router.post('/api/addnewburger', function (req, res) {
+//     query = `INSERT INTO burgers (burger_name, devoured) VALUES ('${req.body.burgerName}', false);`
+//     connection.query(query, function (err, result) {
+//         res.send('Burger added.')
+//     });
+// })
+
+// //PUT ROUTES ####################################################################################
+
+// //Change devoured status
+// router.put('/api/changedevouredstatus', function (req, res) {
+//     console.log(query)
+//     query = `UPDATE burgers SET devoured=${req.body.newdevouredstatus} WHERE id='${req.body.id}';`
+//     connection.query(query, function (err, result) {
+//         res.send('Burger devoured status updated.')
+//     });
+// })
+
+// //DELETE ROUTES ####################################################################################
+
+// //Delete a burger
+// router.delete('/api/deleteaburger', function (req, res) {
+//     query = `DELETE FROM burgers WHERE id='${req.body.id}';`
+//     connection.query(query, function (err, result) {
+//         res.send('Burger deleted.')
+//     });
+// })
 
 module.exports = router;
